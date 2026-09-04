6e7e720f3dc05e6d96e229c8ecf3aaac415d70fc
D4RK-BOULKA1 <boulka2026@gmail.com>
2026-09-04 22:39:45 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,11 +7,13 @@
 // 4. Copie les valeurs qu'on te donne et colle-les ci-dessous
 // ============================================================
 
-export const firebaseConfig = {
+
+const firebaseConfig = {
   apiKey: "AIzaSyBsaeh1OHkhO3VGFrJlZWE0tX1X4IgtelM",
   authDomain: "boulka-bad4c.firebaseapp.com",
   projectId: "boulka-bad4c",
   storageBucket: "boulka-bad4c.firebasestorage.app",
   messagingSenderId: "726705220290",
-  appId: "1:726705220290:web:1652197444da95d4ece95b"
+  appId: "1:726705220290:web:1652197444da95d4ece95b",
+  measurementId: "G-HBZDDT20ME"
 };
