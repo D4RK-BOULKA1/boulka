019c9824ee83b9373d07ee24ea21234d282542ad
D4RK-BOULKA1 <boulka2026@gmail.com>
2026-09-04 23:14:38 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,4 @@
-// ============================================================
-// CONFIGURATION FIREBASE
-// ============================================================
-// 1. Va sur https://console.firebase.google.com
-// 2. Crée un projet (gratuit)
-// 3. Dans le projet : icône </> "Ajouter une application Web"
-// 4. Copie les valeurs qu'on te donne et colle-les ci-dessous
-// ============================================================
-
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBsaeh1OHkhO3VGFrJlZWE0tX1X4IgtelM",
   authDomain: "boulka-bad4c.firebaseapp.com",
   projectId: "boulka-bad4c",
